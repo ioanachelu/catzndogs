244f1bdadef75fd582dcf3da67fc11a43c3d6251
Ioana Chelu <ioana.chelu28@gmail.com>
2017-03-17 20:35:47 +0200

    first solution

--- keras/run.py
+++ keras/run.py
@@ -1,115 +1,77 @@
-'''This script goes along the blog post
-"Building powerful image classification models using very little data"
-from blog.keras.io.
-It uses data that can be downloaded at:
-https://www.kaggle.com/c/dogs-vs-cats/data
-In our setup, we:
-- created a data/ folder
-- created train/ and validation/ subfolders inside data/
-- created cats/ and dogs/ subfolders inside train/ and validation/
-- put the cat pictures index 0-999 in data/train/cats
-- put the cat pictures index 1000-1400 in data/validation/cats
-- put the dogs pictures index 12500-13499 in data/train/dogs
-- put the dog pictures index 13500-13900 in data/validation/dogs
-So that we have 1000 training examples for each class, and 400 validation examples for each class.
-In summary, this is our directory structure:
-```
-data/
-    train/
-        dogs/
-            dog001.jpg
-            dog002.jpg
-            ...
-        cats/
-            cat001.jpg
-            cat002.jpg
-            ...
-    validation/
-        dogs/
-            dog001.jpg
-            dog002.jpg
-            ...
-        cats/
-            cat001.jpg
-            cat002.jpg
-            ...
-```
-'''
-
-from keras import applications
 from keras.preprocessing.image import ImageDataGenerator
-from keras import optimizers
 from keras.models import Sequential
-from keras.layers import Dropout, Flatten, Dense
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Activation, Dropout, Flatten, Dense
+from keras import backend as K
 
-# path to the model weights files.
-weights_path = '../keras/examples/vgg16_weights.h5'
-top_model_weights_path = 'fc_model.h5'
+
 # dimensions of our images.
 img_width, img_height = 150, 150
 
-train_data_dir = 'cats_and_dogs_small/train'
-validation_data_dir = 'cats_and_dogs_small/validation'
+train_data_dir = 'data/train'
+validation_data_dir = 'data/validation'
 nb_train_samples = 2000
 nb_validation_samples = 800
 epochs = 50
 batch_size = 16
 
-# build the VGG16 network
-model = applications.VGG16(weights='imagenet', include_top=False)
-print('Model loaded.')
+if K.image_data_format() == 'channels_first':
+    input_shape = (3, img_width, img_height)
+else:
+    input_shape = (img_width, img_height, 3)
 
-# build a classifier model to put on top of the convolutional model
-top_model = Sequential()
-top_model.add(Flatten(input_shape=model.output_shape[1:]))
-top_model.add(Dense(256, activation='relu'))
-top_model.add(Dropout(0.5))
-top_model.add(Dense(1, activation='sigmoid'))
+model = Sequential()
+model.add(Conv2D(32, (3, 3), input_shape=input_shape))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
 
-# note that it is necessary to start with a fully-trained
-# classifier, including the top classifier,
-# in order to successfully do fine-tuning
-top_model.load_weights(top_model_weights_path)
+model.add(Conv2D(32, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
 
-# add the model on top of the convolutional base
-model.add(top_model)
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
 
-# set the first 25 layers (up to the last conv block)
-# to non-trainable (weights will not be updated)
-for layer in model.layers[:25]:
-    layer.trainable = False
+model.add(Flatten())
+model.add(Dense(64))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
 
-# compile the model with a SGD/momentum optimizer
-# and a very slow learning rate.
 model.compile(loss='binary_crossentropy',
-              optimizer=optimizers.SGD(lr=1e-4, momentum=0.9),
+              optimizer='rmsprop',
               metrics=['accuracy'])
 
-# prepare data augmentation configuration
+# this is the augmentation configuration we will use for training
 train_datagen = ImageDataGenerator(
     rescale=1. / 255,
     shear_range=0.2,
     zoom_range=0.2,
     horizontal_flip=True)
 
+# this is the augmentation configuration we will use for testing:
+# only rescaling
 test_datagen = ImageDataGenerator(rescale=1. / 255)
 
 train_generator = train_datagen.flow_from_directory(
     train_data_dir,
-    target_size=(img_height, img_width),
+    target_size=(img_width, img_height),
     batch_size=batch_size,
     class_mode='binary')
 
 validation_generator = test_datagen.flow_from_directory(
     validation_data_dir,
-    target_size=(img_height, img_width),
+    target_size=(img_width, img_height),
     batch_size=batch_size,
     class_mode='binary')
 
-# fine-tune the model
 model.fit_generator(
     train_generator,
-    samples_per_epoch=nb_train_samples,
+    steps_per_epoch=nb_train_samples // batch_size,
     epochs=epochs,
     validation_data=validation_generator,
-    nb_val_samples=nb_validation_samples)
+    validation_steps=nb_validation_samples // batch_size)
+
+model.save_weights('first_try.h5')
