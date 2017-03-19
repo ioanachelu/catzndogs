13742e12b85f3e93093027d4f19ecde6fe96c62f
Ioana Chelu <ioana.chelu28@gmail.com>
2017-03-19 08:08:25 +0200

    Some small fixes.

--- keras_solution/run_solution_1.py
+++ keras_solution/run_solution_1.py
@@ -20,15 +20,15 @@ input_shape = (img_width, img_height, 3)
 
 model = Sequential()
 model.add(Conv2D(32, (3, 3), input_shape=input_shape))
-model.add(Activation('elu'))
+model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
 model.add(Conv2D(32, (3, 3)))
-model.add(Activation('elu'))
+model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
 model.add(Conv2D(64, (3, 3)))
-model.add(Activation('elu'))
+model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
 model.add(Flatten())
